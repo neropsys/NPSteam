60ac37f11a7a311273503d40382f1a7849609cf9
neropsys <neropsys@gmail.com>
2016-03-13 16:40:26 +0900

    Used name of the folder in common directory for game title instead of window title

--- NPSteam/NPSteam/MainWindow.xaml.cs
+++ NPSteam/NPSteam/MainWindow.xaml.cs
@@ -16,7 +16,9 @@ namespace NPSteam
     public partial class MainWindow
     {
         const string appName = "GameOverlayUI";
-        const string regexPattern = @"(?!-pid)(\d+)\w";
+        const string pidRegex = @"(?!-pid)(\d+)\w";
+        const string overlaydPidQuery = "select CommandLine from Win32_Process where ProcessId = {0}";
+        const string gameDirQueryString = "select ExecutablePath from Win32_Process where ProcessId = {0}";
         string gameName = null;
         public MainWindow()
         {
@@ -33,24 +35,44 @@ namespace NPSteam
             var processList = Process.GetProcessesByName(appName);
             Process overlayApp = getSteamProcess(processList);
 
+
             if (overlayApp == null)
                 return;
 
-            string wmiQuery = string.Format("select CommandLine from Win32_Process where ProcessId = {0}", overlayApp.Id);
-            var searcher = new ManagementObjectSearcher(wmiQuery);
-            var retCollection = searcher.Get();
-            foreach (var retObject in retCollection)
-            {
-                Global.Pid = Regex.Match(retObject["CommandLine"].ToString(), regexPattern).Value;
-                Process steamApp = Process.GetProcessById(Convert.ToInt16(Global.Pid));
 
-                Console.WriteLine("pid = {0}, game Name = {1}", Global.Pid, steamApp.MainWindowTitle);
-                gameName = steamApp.MainWindowTitle;
-                //Global.Service.sendt
+            string wmiQuery = string.Format(overlaydPidQuery, overlayApp.Id);
+            using (var searcher = new ManagementObjectSearcher(wmiQuery))
+            using (var retCollection = searcher.Get())
+            {
+                foreach (var retObject in retCollection)
+                {
+                    //get game pid
+                    Global.Pid = Regex.Match(retObject["CommandLine"].ToString(), pidRegex).Value;
+                    var steamApp = Process.GetProcessById(Convert.ToInt16(Global.Pid));
+
+                    string gameDirQuery = string.Format(gameDirQueryString, steamApp.Id);
+
+                    //get game title from common directory in steamapps
+                    using (var gameSearcher = new ManagementObjectSearcher(gameDirQuery))
+                    using (var retValue = gameSearcher.Get())
+                    {
+                        foreach(var gameProcess in retValue)
+                        {
+                            Console.WriteLine("exe directory : " + gameProcess["ExecutablePath"].ToString());
+                            var directorArray = gameProcess["ExecutablePath"].ToString().Split('\\');
+
+
+                            //get name of the game from directory instead of window title of the game
+                            Console.WriteLine("directory : " + directorArray[directorArray.Length-2]);
+                            gameName = directorArray[directorArray.Length - 2];
+                        }
+
+                    }
+
+
+                    Console.WriteLine("pid = {0}, game Name = {1}", Global.Pid, steamApp.MainWindowTitle);
+                }
             }
-            retCollection.Dispose();
-            searcher.Dispose();
-            
         }
         Process getSteamProcess(Process[] processList)
         {
@@ -71,7 +93,7 @@ namespace NPSteam
             Left = screenArea.Right - Width;
             Top = screenArea.Bottom - Height;
             scanProcess();
-            string tweetFormat = "Now Playing - " + gameName + " On Steam #NPSteam";
+            string tweetFormat = "Now Playing - " + gameName + " on Steam #NPSteam";
             var result = Global.Service.BeginSendTweet(new TweetSharp.SendTweetOptions { Status = tweetFormat });
         }
     }
